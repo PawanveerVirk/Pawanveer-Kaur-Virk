789a7e66cedce5b5b9f90ff394aa337457664de6
Pawanveer virk <pawanveerkaur.virk@dcmail.ca>
2024-03-31 22:50:55 -0400

    Changes In Main.js
    i made changes in the main.js file i added te array of images, text for each image.

--- Assignment-4/Part-2/gallery-start/gallery-start/main.js
+++ Assignment-4/Part-2/gallery-start/gallery-start/main.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg', 'pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText =['A beautiful Lanscapee','A beautiful Lanscapee','A beautiful Lanscapee','A beautiful Lanscapee','A beautiful Lanscapee'];
 
 /* Looping through images */
 
